2cababc46a42632386d1c663a8d14c758be535b5
Diana Zepeda Tatengo <diana.zepeda6085@alumnos.udg.mx>
2025-07-21 13:31:05 -0600

    Refactored player logic into Player class

--- js/MainScene.js
+++ js/MainScene.js
@@ -1,3 +1,5 @@
+import Player from "./Player.js";
+
 // The class can be imported in other files "export default"
 export default class MainScene extends Phaser.Scene {
     constructor() {
@@ -5,18 +7,15 @@ export default class MainScene extends Phaser.Scene {
     }
 
     preload(){     
-        // Load the atlas for the player
-        this.load.atlas('female','assets/images/female.png','assets/images/female_atlas.json');  
-        // Load the animations for the player            
-        this.load.animation('female_anim','assets/images/female_anim.json');
+       Player.preload(this); // Preload the player assets
     }
 
     create(){  
         // create a sprite of the player using the Matter.js physics engine       
-        this.player = new Phaser.Physics.Matter.Sprite(this.matter.world,0,0,'female','townsfolk_f_idle_1');
-        this.add.existing(this.player); // Add the player to the scene
+        this.player = new Player({scene:this,x:0,y:0,texture:'female',frame:'townsfolk_f_idle_1'});
+        //this.add.existing(this.player); // Add the player to the scene
         // Add controls
-        this.inputKeys = this.input.keyboard.addKeys({
+        this.player.inputKeys = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
             left: Phaser.Input.Keyboard.KeyCodes.A,
@@ -25,26 +24,6 @@ export default class MainScene extends Phaser.Scene {
     }
     
     update(){
-        // Play walking animation for the player
-        this.player.anims.play('female_walk',true);
-        // Define the player movement
-        const speed = 2.5;
-        // Velocity vector (0,0)
-        let playerVelocity = new Phaser.Math.Vector2();
-        // Key and direction control
-        if(this.inputKeys.left.isDown) {
-            playerVelocity.x = -1;
-        }else if(this.inputKeys.right.isDown) {
-            playerVelocity.x = 1;
-        }
-        if(this.inputKeys.up.isDown) {
-            playerVelocity.y = -1;
-        }else if(this.inputKeys.down.isDown) {
-            playerVelocity.y = 1;
-        }
-        playerVelocity.normalize(); // normalize the vector to avoid diagonal speed increase
-        playerVelocity.scale(speed); // Multiply the vector by the velocity
-        // Apply the velocity vector to the player's sprite
-        this.player.setVelocity(playerVelocity.x,playerVelocity.y); 
+        this.player.update();
     }
 }
---
+++ js/Player.js
@@ -0,0 +1,39 @@
+export default class Player extends Phaser.Physics.Matter.Sprite {
+    constructor(data){
+        let {scene,x,y,texture,frame} = data;
+        super(scene.matter.world,x,y,texture,frame);
+        this.scene.add.existing(this);        
+    }
+
+    static preload(scene){
+        // Load the atlas for the player
+        scene.load.atlas('female','assets/images/female.png','assets/images/female_atlas.json');
+        // Load the animations for the player
+        scene.load.animation('female_anim','assets/images/female_anim.json');
+    }   
+
+    update(){
+        // Play walking animation for the player
+        this.anims.play('female_walk',true);
+        // Define the player movement
+        const speed = 2.5;
+        // Velocity vector (0,0)
+        let playerVelocity = new Phaser.Math.Vector2();
+        // Key and direction control
+        if(this.inputKeys.left.isDown) {
+            playerVelocity.x = -1;
+        }else if(this.inputKeys.right.isDown) {
+            playerVelocity.x = 1;
+        }
+        if(this.inputKeys.up.isDown) {
+            playerVelocity.y = -1;
+        }else if(this.inputKeys.down.isDown) {
+            playerVelocity.y = 1;
+        }
+        playerVelocity.normalize(); // normalize the vector to avoid diagonal speed increase
+        playerVelocity.scale(speed); // Multiply the vector by the velocity
+
+        // Apply the velocity vector to the player's sprite
+        this.setVelocity(playerVelocity.x,playerVelocity.y);       
+    }   
+}
