d6fda0de2692ed58486ef0b2ccbb18059b740bca
Diana Zepeda Tatengo <diana.zepeda6085@alumnos.udg.mx>
2025-07-21 13:40:16 -0600

    Added conditional animation switching based on player movement

--- js/Player.js
+++ js/Player.js
@@ -10,11 +10,13 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
         scene.load.atlas('female','assets/images/female.png','assets/images/female_atlas.json');
         // Load the animations for the player
         scene.load.animation('female_anim','assets/images/female_anim.json');
-    }   
+    }
 
-    update(){
-        // Play walking animation for the player
-        this.anims.play('female_walk',true);
+    get velocity(){
+        return this.body.velocity;
+    }
+
+    update(){             
         // Define the player movement
         const speed = 2.5;
         // Velocity vector (0,0)
@@ -34,6 +36,13 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
         playerVelocity.scale(speed); // Multiply the vector by the velocity
 
         // Apply the velocity vector to the player's sprite
-        this.setVelocity(playerVelocity.x,playerVelocity.y);       
+        this.setVelocity(playerVelocity.x,playerVelocity.y); 
+
+        // Play walking or idle animation for the player
+        if(Math.abs(this.velocity.x) > 0.1 || (Math.abs(this.velocity.y) > 0.1)){
+            this.anims.play('female_walk',true);
+        }else{
+            this.anims.play('female_idle',true);
+        }
     }   
 }
