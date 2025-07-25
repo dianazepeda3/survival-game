d7de57230ca3eb89359764cbd63a6bef8e5bc80b
Diana Zepeda Tatengo <diana.zepeda6085@alumnos.udg.mx>
2025-07-25 13:14:13 -0600

    Enemy Placement

--- js/Enemy.js
+++ js/Enemy.js
@@ -4,13 +4,16 @@ export default class Enemy extends MatterEntity {
     static preload(scene){
         scene.load.atlas('enemies','assets/images/enemies.png','assets/images/enemies_atlas.json');
         scene.load.animation('enemies_anim','assets/images/enemies_anim.json');
+        scene.load.audio('bear','assets/audio/bear.wav');
+        scene.load.audio('wolf','assets/audio/wolf.wav');
+        scene.load.audio('ent','assets/audio/wolf.wav');
     }
 
     constructor(data){
         let {scene,enemy} = data;
-        let drops = JSON.parse(enemy.propety.find(p=>p.name==='drops'.value));
-        let health = enemy.propety.find(p=>p.name==='health'.value);
-        super({scene,x:enemy.x,y:enemy.y,texture:'enemies',frame:'bear_idle_1',drops,health,name:enemy.name});
+        let drops = JSON.parse(enemy.properties.find(p=>p.name=='drops').value);
+        let health = enemy.properties.find(p=>p.name==='health').value;
+        super({scene,x:enemy.x,y:enemy.y,texture:'enemies',frame:`${enemy.name}_idle_1`,drops,health,name:enemy.name});
     }
 
     update(){
