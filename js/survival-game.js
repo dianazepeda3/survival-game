753c0b6736387fbad75171799fb7ea6599784b36
Diana Zepeda Tatengo <diana.zepeda6085@alumnos.udg.mx>
2025-07-25 14:08:13 -0600

    Quit the debug lines

--- js/survival-game.js
+++ js/survival-game.js
@@ -13,7 +13,7 @@ const config = {
     physics: { // Using Matter.js physics engine
         default: 'matter',
         matter: {
-            debug: true,
+            debug: false,
             gravity:{y:0},
         }
     },
